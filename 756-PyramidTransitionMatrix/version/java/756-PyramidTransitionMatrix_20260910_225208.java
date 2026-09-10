0cf96dd589b9ab856f61ca37b0843aed79f328b8
NITHEESH R <nitheesh25bls@gmail.com>
2026-09-10 22:52:09 +0530

    Create 756-PyramidTransitionMatrix

---
+++ 756-PyramidTransitionMatrix/version/java/756-PyramidTransitionMatrix_20260910_225208.java
@@ -0,0 +1,23 @@
+// Last updated: 9/10/2026, 10:52:08 PM
+1class Solution {
+2    public int[] twoSum(int[] numbers, int target) {
+3        int left = 0;
+4        int right = numbers.length - 1;
+5
+6        while (left < right) {
+7            int sum = numbers[left] + numbers[right];
+8
+9            if (sum == target) {
+10                return new int[]{left + 1, right + 1};
+11            }
+12            else if (sum < target) {
+13                left++;
+14            }
+15            else {
+16                right--;
+17            }
+18        }
+19
+20        return new int[]{-1, -1};
+21    }
+22}
