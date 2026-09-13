cf142bf86aa8519cd564c59bc1c69e7f5140bd04
NITHEESH R <nitheesh25bls@gmail.com>
2026-09-13 23:39:11 +0530

    Create 476-NumberComplement

---
+++ 476-NumberComplement/version/java/476-NumberComplement_20260913_233910.java
@@ -0,0 +1,15 @@
+// Last updated: 9/13/2026, 11:39:10 PM
+1class Solution {
+2    public int findComplement(int num) {
+3        int mask = 0;
+4        int n = num;
+5
+6        while (n > 0) {
+7            mask = (mask << 1) | 1;
+8            n = n >> 1;
+9        }
+10
+11        return num ^ mask;
+12    }
+13}
+14
